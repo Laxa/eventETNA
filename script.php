8c28d15468ca214c2a6df44cad47615ff4908cb7
laxa <L4x4t0r@gmail.com>
2015-06-02 22:29:30 +0200

    Toutes les notes

--- script.php
+++ script.php
@@ -32,9 +32,65 @@ function get($url, &$cookie)
 function main()
 {
     $cookie = trim(file_get_contents('cookie'));
-    $json = array();
     $users = getUsers('https://intra.etna-alternance.net/report/trombi/list/term/Master%20-%20Mars/year/2017', $cookie);
-    var_dump($users);
+    foreach ($users as $user => $id)
+    {
+        echo "Getting notes for $id:$user...";
+        getNotesForUser($id, $cookie);
+        echo "Done!\n";
+    }
+}
+
+function getNotesForUser($id, &$cookie)
+{
+    $userPage = get('https://intra.etna-alternance.net/report/index/summary/id/'.$id, $cookie);
+    /* $userPage = file_get_contents('tmp'); */
+    /* Treating the HTML page */
+    $userPage = html_entity_decode($userPage);
+    /* A changer le jour ou on a un vrai intranet... */
+    $userPage = utf8_encode($userPage);
+    $array = explode('<th class="marks_uv" colspan="5">', $userPage);
+    /* We throw away first elem, cause it's useless one */
+    array_shift($array);
+    $notes = array();
+    while ($uv = array_shift($array))
+    {
+        preg_match('#([^<]+)#', $uv, $match);
+        $notes[trim($match[1])] = getNotesForUv($uv);
+    }
+    file_put_contents('notes/'.$id, json_encode($notes));
+    return $notes;
+}
+
+function getNotesForUv($uv)
+{
+    $notes = array();
+    preg_match_all('#<tr[^>]*>(.*?)</tr#s', $uv, $matches);
+    $tmp = $matches[1];
+    for ($i = 0; $i < sizeof($tmp); $i++)
+    {
+        preg_match_all('#<td[^>]*>(.*?)</td#s', $tmp[$i], $noteRow);
+        /* Ne devrait jamais lever d'exception, mais on ne sait jamais avec EUTEUNA */
+        try
+        {
+            $notes[$i]["date"] = trim(strip_tags($noteRow[1][0]));
+            $notes[$i]["intitule"] = trim(strip_tags($noteRow[1][1]));
+            if ($noteRow[1][2][0] == '/')
+                $notes[$i]["note"] = "NYD";
+            else
+                $notes[$i]["note"] = (int)trim(strip_tags($noteRow[1][2]));
+            $notes[$i]["moyenne"] = trim(strip_tags($noteRow[1][3]));
+            $notes[$i]["commentaire"] = trim(strip_tags($noteRow[1][4]));
+            preg_match("#ref='([^']*)#", $noteRow[1][4], $link);
+            $notes[$i]["link"] = 'https://intra.etna-alternance.net'.$link[1];
+        }
+        catch (Exception $e)
+        {
+            echo "L'intranet est en carton, rien de nouveau jusqu'ici...\n";
+            echo $e->getMessage()."\n";
+        }
+    }
+    return $notes;
 }
 
 function getUsers($url, &$cookie)
@@ -48,6 +104,7 @@ function getUsers($url, &$cookie)
         exit(-1);
     }
     $tmp = $matches[1];
+    /* Pattern to get id from promotion page */
     if (preg_match_all('#summary\/id\/([^\"]+)#', $etna2017, $matches) == 0 || !sizeof($matches[1]))
     {
         echo "Failed to preg_match_all ids on promotion page\n";
@@ -60,3 +117,4 @@ function getUsers($url, &$cookie)
 }
 
 main();
+/* getNotesForUser(42, $toto = 2); */
